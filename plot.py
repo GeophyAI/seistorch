5d47c53f6558357be18c2dc9ef2ad8cb70e68b74
shaowinw <shaowinw@163.com>
2023-07-16 19:53:27 -0400

    More faster by using compile.

--- plot.py
+++ plot.py
@@ -13,12 +13,12 @@ import argparse
 
 # args = parser.parse_args() 
 
-# d = np.load("/mnt/data/wangsw/inversion/elastic_marmousi/data/acoustic_fixed.npy", allow_pickle=True)
+# d = np.load("/mnt/data/wangsw/inversion/elastic_marmousi/data/elastic_fixed.npy", allow_pickle=True)
 
 # print(d.shape)
-# dx = d[...,0]
-# dz = d[...,0]
-# no = 80
+# # dx = d[...,0]
+# # dz = d[...,1]
+# no = 0
 
 # d = d[no]
 # dx = d[...,0]
@@ -26,15 +26,16 @@ import argparse
 # print(dx.shape, dz.shape)
 # fig,axes = plt.subplots(1,2, figsize=(8,5))
 # vmin, vmax = np.percentile(dx, [2, 98])
-# axes[0].imshow(dx.squeeze(), vmin=vmin, vmax=vmax, aspect='auto', cmap=plt.cm.seismic)
+# ax0=axes[0].imshow(dx.squeeze(), vmin=vmin, vmax=vmax, aspect='auto', cmap=plt.cm.seismic)
 # vmin, vmax = np.percentile(dz, [2, 98])
-# axes[1].imshow(dz.squeeze(), vmin=vmin, vmax=vmax, aspect='auto', cmap=plt.cm.seismic)
+# ax1=axes[1].imshow(dz.squeeze(), vmin=vmin, vmax=vmax, aspect='auto', cmap=plt.cm.seismic)
 # plt.show()
+# plt.colorbar(ax0)
 # # # plt.savefig("test_15hz.png")
 # exit()
 
-F = 2
-epoch = 99
+F = 0
+epoch = 90
 PMLN = 50
 dx = dz = 12.5
 EXPAND = 100
@@ -44,13 +45,13 @@ EXPAND = 100
 # #root_path = r"/public1/home/wangsw/FWI/EFWI/Marmousi/marmousi_10m/ss"
 # root_path = r"/mnt/data/wangsw/inversion/marmousi_10m/elastic/cycleskipping/envelope"
 # root_path = r"/mnt/data/wangsw/inversion/overthrust_15m/compare_loss/l2"
-root_path = r"/mnt/data/wangsw/inversion/elastic_marmousi/results/tow/cs"
+root_path = r"/mnt/data/wangsw/inversion/elastic_marmousi/results/fixed/aec/iml1_xz"
 
 #r"/mnt/data/wangsw/inversion/marmousi/elastic/oldcodes"
 loss = root_path.split("/")[-1]
 coding = "."
 grad_vp = np.load(f"{root_path}/{coding}/gradvpF{F:02d}E{epoch:02d}.npy")[PMLN:-PMLN,PMLN:-PMLN]
-grad_vs = np.load(f"{root_path}/{coding}/gradvpF{F:02d}E{epoch:02d}.npy")[PMLN:-PMLN,PMLN:-PMLN]
+grad_vs = np.load(f"{root_path}/{coding}/gradvsF{F:02d}E{epoch:02d}.npy")[PMLN:-PMLN,PMLN:-PMLN]
 print(grad_vs.min(), grad_vs.max(), grad_vp.max(), grad_vp.min())
 
 fig,axes = plt.subplots(1,2, figsize=(10,3))
@@ -100,11 +101,11 @@ init_vp = np.load("/mnt/data/wangsw/inversion/elastic_marmousi/velocity/init_vp_
 
 
 vp = np.load(f"{root_path}/{coding}/paravpF{F:02d}E{epoch:02d}.npy")[PMLN:-PMLN,PMLN+EXPAND:-PMLN-EXPAND]
-vs = np.load(f"{root_path}/{coding}/paravpF{F:02d}E{epoch:02d}.npy")[PMLN:-PMLN,PMLN+EXPAND:-PMLN-EXPAND]
+vs = np.load(f"{root_path}/{coding}/paravsF{F:02d}E{epoch:02d}.npy")[PMLN:-PMLN,PMLN+EXPAND:-PMLN-EXPAND]
 print("vp", vp.shape, vp.max(), vp.min())
 print("vs", vs.shape, vs.max(), vs.min())
 print(vp.shape, true_vp.shape)
-trace = 500
+trace = 1000
 plt.plot(true_vp[:,trace], label="true")
 plt.plot(vp[:,trace], label="inverted")
 plt.plot(init_vp[:,trace], label="init")
@@ -116,8 +117,8 @@ fig,axes = plt.subplots(1,2, figsize=(12,4))
 vmin, vmax = (true_vp.min(), true_vp.max())#(1.5, 5.500)
 # vmin, vmax=vp.max(), vp.min()
 extent = (0, true_vp.shape[1]*dx, true_vp.shape[0]*dz, 0)
-ax0=axes[0].imshow(true_vp, vmin=vmin, vmax=vmax, aspect='auto', extent=extent, cmap=plt.cm.seismic)
-# vmin, vmax = (0, 5500/1.73)#(1.5, 5.500)
+ax0=axes[0].imshow(vp, vmin=vmin, vmax=vmax, aspect='auto', extent=extent, cmap=plt.cm.seismic)
+vmin, vmax = (0, 5500/1.73)#(1.5, 5.500)
 # vmin, vmax = (true_vs.min(), true_vs.max())#(1.5, 5.500)
 ax1=axes[1].imshow(vs, vmin=vmin, vmax=vmax, aspect='auto', extent=extent, cmap=plt.cm.seismic)
 axes[0].set_title(loss+"_vp")
