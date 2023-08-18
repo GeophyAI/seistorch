10ccd96b8021b74134ec7c737173542969302098
shaowinw <shaowinw@163.com>
2023-08-18 09:27:49 +0800

    Update roll & intersection for arrival mask.

--- coding.sh
+++ coding.sh
@@ -1,12 +1,12 @@
 export LD_LIBRARY_PATH=/home/wangsw/anaconda3/lib:$LD_LIBRARY_PATH && \
-python codingfwi.py config/multiple.yml \
+python codingfwi.py config/test_source_inversion2/obs.yml \
 --gpuid 0 \
 --opt adam \
 --loss vp=l2 \
 --mode inversion \
 --batchsize 20 \
 --lr vp=10.0 \
---save-path /mnt/data/wangsw/inversion/elastic_marmousi/results/fixed/multiple/l2_linear \
+--save-path /public1/home/wangsw/FWI/EFWI/Marmousi/Marmousi2/results/diff_wavelet/l2_true \
 --checkpoint /mnt/data/wangsw/inversion/marmousi_10m/elastic/compare_loss2/l2_newopt/ckpt_9.pt \
 --use-cuda \
 --grad-cut
