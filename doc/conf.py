223cfda47a977200fba2e75e2e1a4150f1f98dcf
shaowinw <shaowinw@163.com>
2023-09-09 10:21:38 +0800

    Fixed

--- doc/conf.py
+++ doc/conf.py
@@ -35,8 +35,7 @@ source_suffix = ['.rst', '.md']
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 mathjax_path = 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
-extensions = ['sphinx.ext.mathjax',
-              'sphinxcontrib.katex',
+extensions = [
 ]
 
 
